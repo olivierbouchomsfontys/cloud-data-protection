9e3ac0d5e1bacd499abe94b5e1d3e514ec807c3f
Olivier Bouchoms <o.bouchoms@student.fontys.nl>
2021-04-19 11:10:31 +0200

    Update cloud-data-protection/src/features/userSlice.tsx

--- cloud-data-protection/src/features/userSlice.tsx
+++ cloud-data-protection/src/features/userSlice.tsx
@@ -1,4 +1,4 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {LoginResult} from "../services/result/loginResult";
 
 export const userSlice = createSlice({
