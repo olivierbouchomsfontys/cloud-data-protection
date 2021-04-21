d71197bd38947ee4ced88d749661332687ffd55d
Olivier Bouchoms <391028@student.fontys.nl>
2021-04-21 09:06:26 +0200

    Add intro texts for onboarding

--- cloud-data-protection/src/components/onboarding/onboarding.tsx
+++ cloud-data-protection/src/components/onboarding/onboarding.tsx
@@ -43,6 +43,21 @@ const OnboardingComponent = () => {
                 <Typography variant='subtitle2' className='onboarding__status'>Status: {OnboardingStatus[onboarding!.status].toLowerCase()}</Typography> :
                 <Typography variant='subtitle2' className='onboarding__status'>Status: loading</Typography>
             }
+            {onboarding?.status === OnboardingStatus.None &&
+                <p>
+                    Get started by connecting your company Google Workspace account.
+                </p>
+            }
+            {onboarding?.status === OnboardingStatus.AccountConnected &&
+                <p>
+                    Congratulations! Your Google Workspace account is connected. Please choose a backup scheme that fits your business.
+                </p>
+            }
+            {onboarding?.status === OnboardingStatus.Complete &&
+                <p>
+                    Congratulations! Your data is being secured by Cloud Data Protection.
+                </p>
+            }
         </div>
     )
 
