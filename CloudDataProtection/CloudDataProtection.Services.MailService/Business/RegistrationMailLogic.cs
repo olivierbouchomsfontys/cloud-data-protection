7d82c968674bbcc4b1a1ca5416efaacfdfd258f6
Olivier Bouchoms <391028@student.fontys.nl>
2021-04-28 09:32:03 +0200

    Add mail template and update mail styling

--- CloudDataProtection/CloudDataProtection.Services.MailService/Business/RegistrationMailLogic.cs
+++ CloudDataProtection/CloudDataProtection.Services.MailService/Business/RegistrationMailLogic.cs
@@ -7,6 +7,13 @@ namespace CloudDataProtection.Services.MailService.Business
     {
         private readonly IMailSender _sender;
 
+        private const string Template = @"
+<div style='width: max(40%, 480px); font-family:sans-serif;'>
+  <div style='background-color: #3f51b5; display: block; font-family: sans-serif; color: #fff; padding: 1rem; box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)'>Cloud Data Protection</div>
+  {0}
+</div>
+";
+
         public RegistrationMailLogic(IMailSender sender)
         {
             _sender = sender;
@@ -15,14 +22,13 @@ namespace CloudDataProtection.Services.MailService.Business
         public async Task SendUserRegistered(string email)
         {
             string subject = "Welcome to Cloud Data Protection";
-            string body = @"Dear Sir / Madam,
-
-Congratulations! You just completed the first step to securing all your company data. Please log in to your account and complete your registration.
-
-Yours sincerely,
-
-Olivier Bouchoms
-";
+            string content = @"
+<p>Dear Sir / Madam,</br></br>
+    Congratulations! You just completed the first step to securing all your company data. Please log in to your account and complete your registration.</br></br>
+    Yours sincerely,</br></br>
+    Olivier Bouchoms
+  </p>";
+            string body = string.Format(Template, content);
 
             await _sender.Send(email, subject, body);
         }
