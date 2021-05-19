cc2daf4ea02b9de98bbd611a51274510eb2d3115
Olivier Bouchoms <391028@student.fontys.nl>
2021-05-19 11:05:40 +0200

    Display swagger on test environment

--- CloudDataProtection/CloudDataProtection.Gateway/Startup.cs
+++ CloudDataProtection/CloudDataProtection.Gateway/Startup.cs
@@ -74,7 +74,7 @@ namespace CloudDataProtection
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Test"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
