26e18c280464edbd19b16ce22117161124d7882a
Olivier Bouchoms <391028@student.fontys.nl>
2021-06-06 19:12:05 +0200

    Log Entity Framework query performance

--- CloudDataProtection/CloudDataProtection.Core/Data/Context/EncryptedDbContextBase.cs
+++ CloudDataProtection/CloudDataProtection.Core/Data/Context/EncryptedDbContextBase.cs
@@ -4,16 +4,21 @@ using CloudDataProtection.Core.Cryptography.Attributes;
 using CloudDataProtection.Core.Data.Converters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
 
 namespace CloudDataProtection.Core.Data.Context
 {
     public abstract class EncryptedDbContextBase : DbContext
     {
+        private static readonly ILoggerFactory LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder => 
+            builder
+                .AddConsole()
+                .AddFilter("Microsoft.EntityFrameworkCore.Migrations", LogLevel.None));
+
         private readonly ITransformer _transformer;
 
         public EncryptedDbContextBase()
         {
-            
         }
 
         public EncryptedDbContextBase(DbContextOptions options, ITransformer transformer) : base(options)
@@ -30,13 +35,14 @@ namespace CloudDataProtection.Core.Data.Context
             {
                 return;
             }
-            
+
             foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
             {
                 foreach (IMutableProperty property in entityType.GetProperties())
                 {
-                    object attributes = property.PropertyInfo.GetCustomAttributes(typeof(EncryptAttribute), false).FirstOrDefault();
-                    
+                    object attributes = property.PropertyInfo.GetCustomAttributes(typeof(EncryptAttribute), false)
+                        .FirstOrDefault();
+
                     if (attributes is EncryptAttribute encryptAttribute)
                     {
                         switch (encryptAttribute.DataType)
@@ -52,16 +58,18 @@ namespace CloudDataProtection.Core.Data.Context
                 }
             }
         }
-        
+
         protected sealed override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             base.OnConfiguring(builder);
+
+            builder.UseLoggerFactory(LoggerFactory);
 
             if (builder.IsConfigured)
             {
                 return;
             }
-            
+
             ConfigureForEfCoreTools(builder);
         }
 
@@ -70,6 +78,5 @@ namespace CloudDataProtection.Core.Data.Context
         /// </summary>
         /// <param name="builder"></param>
         protected abstract void ConfigureForEfCoreTools(DbContextOptionsBuilder builder);
-
     }
 }
