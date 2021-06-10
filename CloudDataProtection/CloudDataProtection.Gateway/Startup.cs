98babfbdc918d94f486adca5cedb8cdc70ca5139
Olivier Bouchoms <391028@student.fontys.nl>
2021-06-10 14:32:34 +0200

    Test gateway pipeline

--- CloudDataProtection/CloudDataProtection.Gateway/Startup.cs
+++ CloudDataProtection/CloudDataProtection.Gateway/Startup.cs
@@ -36,7 +36,7 @@ namespace CloudDataProtection
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
